9d7a5f8cf30e3a4474fcd1bd3fc3d80f4c2f4a0d
siam114 <smsiamislam598@gmail.com>
2026-09-03 21:42:11 +0600

    set up react file and creat todo component and learn props dealink

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Todo from './Todo'
+
+function App() {
+
+  return (
+    <>
+     <h1>Siam App</h1>
+     <Todo task="Buy groceries" time="5:00 PM" />
+     <Todo task="Practice" />
+    </>
+  )
+}
+
+export default App
---
+++ src/Todo.tsx
@@ -0,0 +1,24 @@
+interface TodoPropsProps {
+    task: string;
+    time?: string;
+}
+
+// function Todo(props: TodoPropsProps){
+//     return (
+//           <li>Do this work : {props.task} at {props.time}</li>
+//     )
+// }
+
+function Todo({task,time}: TodoPropsProps){
+    return (
+          <li>Do this work : {task} by {time}</li>
+    )
+}
+
+// function Todo({task,time}){
+//     return (
+//           <li>Do this work : {task} by {time}</li>
+//     )
+// }
+
+export default Todo
