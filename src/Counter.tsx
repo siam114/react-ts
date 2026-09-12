85fa372552a4a8e0b7f03d7115580909fccb068d
siam114 <smsiamislam598@gmail.com>
2026-09-12 15:42:20 +0600

    learn use state

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+
 export default function Counter() {
+
+    const [count, setCount] = useState(0);
+
+    const handleIncrease = () =>{
+        setCount(count + 1);
+    }
+
+    const handleDecrease = () =>{
+        setCount(count - 1);
+    }
+
     return (
         <div>
             <h1>Counter</h1>
-            <p>Current count: 0</p>
-            <button>Increase</button>
+            <p>Current count: {count}</p>
+            <button onClick={handleIncrease}>Increase</button>
+            <button onClick={handleDecrease}>Decrease</button>
         </div>
     )
 }
